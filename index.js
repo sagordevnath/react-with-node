95a3e3b07a0e08c418feaad82c3012de4ce09cf0
sagor devnath <ssagor64@gmail.com>
2022-04-20 18:21:22 +0600

    ready to send data

--- index.js
+++ index.js
@@ -20,8 +20,13 @@ const users = [
     {id: 7, name: 'Messi', email: 'messi@gmail.com', phone: '123-456-7890'},
 ];
 
+// app.get('/user', (req, res) => {
+//     res.send(users);
+// })
+
+
+// filter by search query parameter
 app.get('/users', (req, res) => {
-    // filter by search query parameter
     if (req.query.name) {
         const search = req.query.name;
         const matched = users.filter(user => user.name.toLowerCase().includes(search.toLowerCase()));
